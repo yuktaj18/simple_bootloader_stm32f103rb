2f1dc3e81550e18aeb8d8ff3ecfcde0f432c4ff2
Yukta Jain <yuktaj18@gmail.com>
2026-04-04 15:59:50 +0530

    Added new functions

--- my-common-code/api.h
+++ my-common-code/api.h
@@ -1,1 +1,12 @@
-int my_func(int a);
+#include "libopencm3/stm32/rcc.h"
+#include "libopencm3/cm3/systick.h"
+#include "libopencm3/cm3/vector.h"
+#include "libopencm3/cm3/common.h"
+#include "libopencm3/cm3/vector.h"
+
+#define AHB_FREQ  			(8000000)
+#define SYSTICK_FREQ 		(1000)
+
+
+void systick_setup(void);
+uint64_t get_ticks(void);
