bba3e149aa4c0e721176daf50fe60fa59339ed1f
Yukta Jain <yuktaj18@gmail.com>
2026-04-04 15:58:50 +0530

    Added new functions

--- my-common-code/api.c
+++ my-common-code/api.c
@@ -1,7 +1,23 @@
 
 #include "api.h"
 
-int my_func(int a)
+volatile uint64_t ticks = 0;
+
+void systick_setup(void)
 {
-	return a * 3;
+	systick_set_frequency (SYSTICK_FREQ,AHB_FREQ);
+	systick_counter_enable();
+	systick_interrupt_enable();
 }
+
+
+void sys_tick_handler(void){
+	ticks++;
+}
+
+uint64_t get_ticks(void)
+{
+	return ticks;
+}
+
+
